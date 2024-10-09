b2360dc975eb458a6c0abb2e5b5ab265be59d8c2
William Dias <smdwilliam89@gmail.com>
2024-10-09 14:14:51 -0300

    feat: Implementando as Views e Values.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,37 @@
-console.log('Está funcionando');
+const state = {
+    view: {
+        squares: document.querySelectorAll('.square'),
+        enemy: document.querySelector('.enemy'),
+        timeLeft: document.getElementById('time-left'),
+        score: document.getElementById('score'),
+    },
+    values: {
+        timerId: null,
+        gameSpeed: 1000,
+    },
+};
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove('enemy');
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+
+    randomSquare.classList.add('enemy');
+}
+
+function moveEnemy() {
+    state.values.timerId = setInterval(randomSquare, state.values.gameSpeed);
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {});
+}
+
+function init() {
+    moveEnemy();
+}
+
+init();
